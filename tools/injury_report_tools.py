ff1ae418eb53bdbb52cf0e8e8fa95b169f1db957
Jeremy Smith <jeremysmith@M3-To-the-Max.local>
2025-11-09 08:06:41 -0800

    Let's see if these updates work in production on the bigger model

--- tools/injury_report_tools.py
+++ tools/injury_report_tools.py
@@ -19,8 +19,12 @@ def get_injury_report_for_teams(teams: list) -> dict:
 	injury_reports = []
 	dca = NFLDepthChartAnalyzer()
 	lu = Lookup()
+	i = 0
 	for team in teams:
+		if i >= 3:
+			break
 		if team not in requested_teams:
+			i += 1
 			requested_teams.append(team)
 			print(f"Generating detailed injury report for { team }")
 			try:
