60937704cf5bb62c3a054ab0d273bace7502a86b
Jeremy Smith <jeremysmith@m3-to-the-max.local>
2025-11-11 11:57:27 -0800

    Fixing my bugs in the Features helper

--- helpers/Features.py
+++ helpers/Features.py
@@ -1,9 +1,11 @@
 class Features:
 	def __init__(self):
-		self.base_features = features = [
+		self.base_features = [
 			'elo_rating',
 			'rpi_rating',
-			'days_rest',
+			'days_rest']
+			
+		self.windowed_features = [
 			'avg_points_scored',
 			'avg_pass_adjusted_yards_per_attempt',
 			'avg_rushing_yards_per_attempt',
@@ -20,10 +22,10 @@ class Features:
 		self.extended_features = self.__get_extended_features()
 	
 	def __get_extended_features(self):
-		ef = []
-		for feature in self.base_features:
+		ef = self.baase_features
+		for feature in self.extended_features:
 			for interval in [3, 5, 7]:
-				ef.append(f"avg_{ feature }_l{ interval }")
+				ef.append(f"{ feature }_l{ interval }")
 			for location in ['home', 'away']:
-				ef.append(f"avg_{ feature }_{ location }")
+				ef.append(f"{ feature }_{ location }")
 		return ef
