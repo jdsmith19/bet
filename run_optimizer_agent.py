ea752d2214c67ae8e0a1196989fdc6f51755c6ed
Jeremy Smith <jeremysmith@M3-To-the-Max.local>
2025-11-11 08:46:31 -0800

    Commit before branching

--- run_optimizer_agent.py
+++ run_optimizer_agent.py
@@ -2,7 +2,7 @@ import config
 from agents.OptimizerOrchestrationAgent import OptimizerOrchestrationAgent
 
 agent = OptimizerOrchestrationAgent(
-	model_name = config.adjustment_model,
+	model_name = config.planning_model,
 	max_experiments = 500
 )
 agent.run()
