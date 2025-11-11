55786779b26823d37e3a718300fe8f6e8e7a5fd3
Jeremy Smith <jeremysmith@m3-to-the-max.local>
2025-11-11 14:19:08 -0800

    more fix attempt

--- data_aggregation.py
+++ data_aggregation.py
@@ -1,4 +1,4 @@
 from DataAggregate.DataAggregate import DataAggregate
 
-da = DataAggregate(odds_api_key="57d81d16fc6c36b35f5d8bb5893e1d3d", False)
+da = DataAggregate(odds_api_key="57d81d16fc6c36b35f5d8bb5893e1d3d", save_api_keys=False)
 #print(da.team_performance_with_opponents)
